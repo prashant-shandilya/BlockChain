d85aa62e52ee25e11a408bb899c714d95162c90f
prashant77812 <91403042+prashant77812@users.noreply.github.com>
2022-03-25 00:12:04 +0530

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -5,10 +5,12 @@ import json
 class Blockchain:
 
     def __init__(self):
+        #chain contains the blocks of the blockChain.
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
+        #Proof contains proof 
         block = {'index': len(self.chain) + 1 , 
                 'timestamp':str(datetime.datetime.now()),
                 'proof': proof,
@@ -25,6 +27,7 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
+            #this is the problem to be solved by the miners.
             hash_operation =  hashlib.sha256(str(new_proof**2-previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
